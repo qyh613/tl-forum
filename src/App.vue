f6426619fdbc0161198e896cdd8c5d6565125c0d
qyh <1050258124@qq.con>
2020-07-15 22:22:59 +0800

    删除测试文件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div id="app">
-      手是
-      <van-button type="primary">主要按钮</van-button>
-      <van-button type="warning">警告按钮</van-button>
 
-      <van-icon name="chat-o" color="#1989fa" />
-      <van-icon name="location-o" />
-      <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
