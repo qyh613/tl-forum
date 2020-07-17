ce289c20d10db715d7c0e274a7d49265dd25d2a8
qyh <1050258124@qq.con>
2020-07-17 11:54:51 +0800

    设置登录成功后跳转的问题

--- src/layout/IndexLayout.vue
+++ src/layout/IndexLayout.vue
@@ -2,7 +2,7 @@
 
     <!-- 通用样式 +++++ 路由的跳转 -->
     <div>
-        <router-view />
+        <router-view/>
 
         <van-tabbar route>
             <van-tabbar-item replace to="/index" icon="home-o">
@@ -11,7 +11,8 @@
             <van-tabbar-item replace icon="add">
                 发布
             </van-tabbar-item>
-            <van-tabbar-item replace to="/mine" icon="friends-o">
+<!--                        <van-tabbar-item replace to="/mine" icon="friends-o">-->
+            <van-tabbar-item replace icon="friends-o" @click="IsLogIn">
                 我的
             </van-tabbar-item>
         </van-tabbar>
@@ -20,18 +21,33 @@
 </template>
 
 <script>
+    import {mapState} from "vuex";
+
     export default {
         name: "IndexLayout",
         data() {
             return {
                 show: false
             }
+        },
+        methods: {
+            IsLogIn() {
+                if(this.isLogin){
+                    this.$router.push("/mine/personalDetails")
+                }else {
+                    this.$router.push("/mine/login")
+                }
+            }
+        },
+
+        computed:{
+            ...mapState(["isLogin"])
         }
     }
 </script>
 
 <style scoped lang="less">
-    /deep/.van-icon-add::before {
+    /deep/ .van-icon-add::before {
         color: #3483f6;
         font-size: 35px;
     }
