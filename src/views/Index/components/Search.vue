7f7ee8d56a634773edfdf01e530bbe5e3e6972cb
qyh <1050258124@qq.con>
2020-07-20 10:36:45 +0800

    搜索历史完成

--- src/views/Index/components/Search.vue
+++ src/views/Index/components/Search.vue
@@ -2,10 +2,14 @@
     <div>
         <form action="/">
             <van-search v-model="keyWord" show-action placeholder="请输入搜索关键词"
-                    @cancel="onCancel" autofocus type="search" @focus="value"/>
+                    @cancel="onCancel" autofocus type="search" @search="search"/>
         </form>
         <div class="history">
-            这是搜索历史
+            <div v-for="(item,index) in list" :key="index">
+                <span>{{item}}</span>
+                <van-icon name="delete" @click="delRecord(index)"/>
+            </div>
+            <div class="all"><span @click="removeAll">清除全部</span></div>
         </div>
         <Recommend :keyWord="keyWord"/>
     </div>
@@ -25,25 +29,56 @@
         data() {
             return {
                 keyWord: '',
+                list:[]
             };
         },
         methods: {
             onCancel() {
                 this.$router.push("/index")
-                // console.log(input)
-
             },
-            value(){
-                // console.log(this.keyWord)
-                // getPostSearch(value).then(res=>{
-                //     console.log(res)
-                // })
+            search(){
+                console.log(this.keyWord)
+                this.list.push(this.keyWord)
+                console.log(this.list)
+                localStorage.setItem("keyWord",JSON.stringify(this.list))
+            },
+            // 删除一个
+            delRecord(index){
+                this.list.splice(index,1)
+                localStorage.setItem("keyWord",JSON.stringify(this.list))
+            },
+            // 删除所有
+            removeAll(){
+                localStorage.removeItem("keyWord")
+                this.list=[]
             }
-
         },
+        created() {
+            this.list=JSON.parse(localStorage.getItem("keyWord"))
+            if(this.list==null){
+                this.list=[]
+            }
+        }
     }
 </script>
 
 <style scoped lang="less">
-
+    .history {
+        padding: 0 12px;
+        i{
+            float: right;
+            &:hover{
+                cursor: pointer;
+            }
+        }
+    }
+    .all {
+        text-align: center;
+        padding: 10px 0;
+        span{
+            &:hover{
+                cursor: pointer;
+            }
+        }
+    }
 </style>
